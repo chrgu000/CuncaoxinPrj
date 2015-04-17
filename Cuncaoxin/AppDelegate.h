4c9205cd76456d796ea8d2c70322426ed333cc52
“000000jerry” <761797477@qq.com>
2015-04-17 12:09:14 +0800

    Initial Commit

---
+++ Cuncaoxin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cuncaoxin
+//
+//  Created by 刘杰cjs on 15/4/17.
+//  Copyright (c) 2015年 com.js.cuncaoxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cuncaoxin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cuncaoxin
+//
+//  Created by 刘杰cjs on 15/4/17.
+//  Copyright (c) 2015年 com.js.cuncaoxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
