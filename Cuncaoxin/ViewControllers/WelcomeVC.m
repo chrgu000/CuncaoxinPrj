899a2a3856b6d6316d876517e2fe5e0084634d8d
“000000jerry” <761797477@qq.com>
2015-04-27 17:09:02 +0800

    11

---
+++ Cuncaoxin/ViewControllers/WelcomeVC.m
@@ -0,0 +1,27 @@
+//
+//  WelcomeVC.m
+//  Cuncaoxin
+//
+//  Created by 刘杰cjs on 15/4/21.
+//  Copyright (c) 2015年 com.js.cuncaoxin. All rights reserved.
+//
+
+#import "WelcomeVC.h"
+
+@interface WelcomeVC ()
+
+@end
+
+@implementation WelcomeVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
+    
